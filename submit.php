5ed1491f29426815b269de3552d1dacaca8b7000
Daria <dariashvydka@gmail.com>
2017-08-07 10:41:54 +0300

    submit form part6

--- submit.php
+++ submit.php
@@ -7,22 +7,17 @@ $c = true;
 
 $translate_arr = array(
     "name"  => "Имя",
-    "tel"   => "Телефон",
-    "theme" => "Персонал",
-    "email" => "E-mail",
+    "e-mail" => "E-mail",
+    "text" => "Сообщение",
 );
-var_dump($_POST);
-exit;
+
 foreach ( $_POST as $key => $value ) {
-    if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-        if(empty($key)) $key = "Запись на консультацию";
         $message .= "
 			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
 			<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$translate_arr[$key]</b></td>
 			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
 		</tr>
 		";
-    }
 }
 
 $message = "<table style='width: 100%;'>$message</table>";
